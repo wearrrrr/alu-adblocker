f1915fb8de218d70815e202a8b19d6561fae5a18
wearrrrr <contact@wearr.dev>
2024-07-08 15:11:01 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+const { Engine, RuleStorage, StringRuleList, Request } = require("@adguard/tsurlfilter");
+
+let engine = undefined;
+
+self.adblockExt = {};
+
+async function init() {
+    return new Promise(async (resolve, reject) => {
+        let lists = [
+            {
+                url: "/blocklist?url=https%3A%2F%2Fgithub.com%2Feasylist%2Feasylist%2Fraw%2Fmaster%2Feasyprivacy%2Feasyprivacy_general.txt",
+                title: "EasyPrivacy",
+            },
+            {
+                url: "/blocklist?url=https%3A%2F%2Fgithub.com%2Fjerryn70%2FGoodbyeAds%2Fraw%2Fmaster%2FFormats%2FGoodbyeAds-AdBlock-Filter.txt",
+                title: "GoodbyeAds-Adblock-Filter",
+            },
+            {
+                url: "/blocklist?url=https%3A%2F%2Fgithub.com%2Fjerryn70%2FGoodbyeAds%2Fraw%2Fmaster%2FFormats%2FGoodbyeAds-YouTube-AdBlock-Filter.txt",
+                title: "GoodbyeAds-YouTube-AdBlock-Filter",
+            },
+        ]
+    
+        let parsedLists = [];
+        let index = 0;
+    
+        for (let list of lists) {
+            const text = await fetch(list.url).then(response => response.text());
+            index++;
+            const ruleList = new StringRuleList(index, text, false, false);
+            parsedLists.push(ruleList);
+        }
+
+        try {
+            const ruleStorage = new RuleStorage(parsedLists);
+            engine = new Engine(ruleStorage);
+            resolve();
+        } catch {
+            console.error("Failed to fetch blocklists.");
+            reject();
+        }
+    });
+}
+
+async function filterRequest(fetchEvent) {
+    if (!fetchEvent.request.url.includes("/service/")) {
+        return;
+    }
+    if (!(engine instanceof Engine) || engine === undefined) {
+        await init();
+    }
+
+    const url = fetchEvent.request.url.includes("/service/") ? self.__uv$config.decodeUrl(fetchEvent.request.url.split("/service/")[1]) : fetchEvent.request.url
+    const request = new Request(url);
+    const result = engine.matchRequest(request);
+    if (result.basicRule !== null) {
+        console.log("Blocking request to " + url + " because of rule " + result.basicRule.ruleText);
+        return null;
+    }
+    return result;
+}
+
+self.adblockExt.filterRequest = filterRequest;
